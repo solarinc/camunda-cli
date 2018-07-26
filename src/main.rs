dbb9bbf52dd23377f2f250b86a2deae268839ce1
Maximb <sprayrules@gmail.com>
2018-07-26 02:01:18 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+extern crate camunda_client;
+
+use std::env;
+use camunda_client::api;
+
+fn main() {
+    match env::args().nth(1) {
+        Some(d) => {
+            let url = "http://127.0.0.1:8080/engine-rest/deployment/create";
+            api::deployment::create(url, &d);
+        }
+        None => println!("Nothing passed!")
+    }
+}
